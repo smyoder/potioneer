99d6e33dc15df2284ff172cf4b6f9afa3d81a147
Spencer Yoder <syoder@eurosoftinc.com>
2024-04-29 17:58:25 -0400

    Added inventory bar and workshop arrows

--- js/view/workshop.js
+++ js/view/workshop.js
@@ -4,6 +4,13 @@ function initInventory(inventory) {
   for(let i = 0; i < inventory.length; i++) {
     let slotDOM = document.createElement("span");
     slotDOM.classList.add("inventory-slot");
+    slotDOM.classList.add("drag-target");
+    slotDOM.ondragover = function(ev) {
+      ev.preventDefault();
+    }
+    slotDOM.ondrop = function(ev) {
+      dropIngredient(ev, slotDOM);
+    }
     inventoryDOM.appendChild(slotDOM);
   }
 }
